613757cc16e2e8cdfa718decf36d838cd144b39e
khoa.le <khoa.le@deeptech.com.vn>
2026-02-03 18:00:49 +0700

    update

---
+++ app/katakana/page.tsx
@@ -0,0 +1,11 @@
+import KataFlashCardCtn from "@/containers/katakana/FlashCard/KataFlashCardCtn";
+
+export const page = () => {
+  return (
+    <div className="pb-[10px] md:pb-0">
+      <KataFlashCardCtn />
+    </div>
+  );
+};
+
+export default page;
